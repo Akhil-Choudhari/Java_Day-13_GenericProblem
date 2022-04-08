03488bf5bbbd090f5dc28cbfa4ce7ccc6cd713c5
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-04-09 00:12:34 +0530

    Finding the maximum out of given three integer

---
+++ GenericInegerMax.java
@@ -0,0 +1,20 @@
+package GenericMaxValue;
+
+public class GenericInegerMax {
+
+	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+	    T max = x; 
+
+	    if (y.compareTo(max) > 0)
+	      max = y; 
+
+	    if (z.compareTo(max) > 0)
+	      max = z; 
+
+	    return max; 
+	  } 
+
+	  public static void main(String args[]) {
+	    System.out.printf("Maximum out of %d, %d and %d is %d\n\n", 14, 11, 9, maximum(14, 9, 11));
+}
+}
