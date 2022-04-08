e275277b722432c1bcfbe48d24243278c402132f
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-04-09 00:38:29 +0530

    find maximum out of given three string values

---
+++ GenericStringMax.java
@@ -0,0 +1,23 @@
+package GenericMaxValue;
+
+public class GenericStringMax {
+
+	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+	    T max = x; 
+
+	    if (y.compareTo(max) > 0)
+	      max = y; 
+
+	    if (z.compareTo(max) > 0)
+	      max = z; 
+
+	    return max; 
+	  } 
+
+	  public static void main(String args[]) {
+	    System.out.printf("Maximum of %s, %s and %s is %s\n", "pear", "apple", "orange", maximum(
+	            "pear", "apple", "orange"));
+}
+}
+
+
